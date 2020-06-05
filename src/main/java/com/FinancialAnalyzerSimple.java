d2a303fca0dcad29b1c2772c7f25f18716eea4ce
NotMyThingy <miska.janhunen@protonmail.com>
2020-06-05 21:19:09 +0300

    extracted CSV-parser class

--- src/main/java/com/FinancialAnalyzerSimple.java
+++ src/main/java/com/FinancialAnalyzerSimple.java
@@ -10,18 +10,19 @@ public class FinancialAnalyzerSimple {
 
     private static final String RESOURCES = "src/main/resources/";
 
-    public static void main(final String... args) throws IOException {
+    public static void main(final String... args) throws Exception {
 
-        final Path path = Paths.get(RESOURCES + "resources.csv");
+        final Path path = Paths.get(RESOURCES + args[0]);
         final List<String> lines = Files.readAllLines(path);
 
-        double total = 0d;
+        double total = 0;
         for (final String line : lines) {
             final String[] columns = line.split(",");
             final double amount = Double.parseDouble(columns[1]);
+
             total += amount;
         }
 
-        System.out.println("The total for all transactions is " + total);
+        System.out.println("The total of all transactions is " + total);
     }
 }
